a15fda35c9d70abb30977742905afd8bcf9b280e
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-26 19:07:26 +0300

    add functuins

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('helio')
+const cols = document.querySelectorAll('.col')
+
+function generateColor() {
+  // FF0000
+  // 00FF00
+  // 0000FF
+
+  const hex = '0123456789ABCDEF'
+  let color = ''
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * hex.length)]
+  }
+  return '#' + color
+}
+
+generateColor()
+
+function setRandomColor() {
+  cols.forEach((item) => {})
+}
+
+setRandomColor()
